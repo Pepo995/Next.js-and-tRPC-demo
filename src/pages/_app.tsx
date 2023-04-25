0dd81c54d2b91babe847d6c2284eed2fa02a4da8
Pepo995 <pepoborras@gmail.com>
2023-04-25 08:58:13 -0300

    Next.js and tRPC demo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
+import { api } from "demo/utils/api";
+
+import "demo/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <ClerkProvider {...pageProps}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ClerkProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "demo/server/api/trpc";
+import clerkClient, { type User } from "@clerk/clerk-sdk-node";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profilePicture: user.profileImageUrl,
+  };
+};
+
+export const postsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    // console.log(users);
+
+    const element = posts.map((post) => ({
+      post,
+      author: users.find((user) => user.id === post.authorId),
+    }));
+
+    console.log(element);
+
+    return element;
+  }),
+
+  //   getSecretMessage: protectedProcedure.query(() => {
+  //     return "you can now see this secret message!";
+  //   }),
+});
